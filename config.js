6898011fa048cd050ebfb4ad8f360ce98699f3a6
dinghao1994 <dinghao360@163.com>
2019-06-09 15:15:41 +0800

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+const Config = {
+    AccessKey: '_CghcYndvysaLVqfLaDKc80StCcwOWlxXS4MwAB2',
+    SecretKey: 'X3DSDndHKOMzmms3dh886XwzTiC9t3pHWpkHav0b'
+};
+
+mnodule.export = Config;
+
+
+
+//导出配置文件
